1b3cd93b22ad9082aafa0858ff3a36ce8d4822bd
ness <shonness98@gmail.com>
2025-11-10 20:01:05 +0500

    bump version to `0.1.42.post2`

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytoniq",
-    version="0.1.42.post1",
+    version="0.1.42.post2",
     author="Maksim Kurbatov",
     author_email="cyrbatoff@gmail.com",
     description="TON Blockchain SDK",
